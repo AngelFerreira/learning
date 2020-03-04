8aba421acc07490edc3e5340a1f04aa743b37e36
Angelica <ddperry.af@gmail.com>
2020-03-04 12:20:40 -0300

    Primeira aula

---
+++ Aula01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes =
+            {
+                "Sol",
+                "Lua",
+                "Estrela",
+                "Mar",
+                "Ondas"
+            };
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine(nomes[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
